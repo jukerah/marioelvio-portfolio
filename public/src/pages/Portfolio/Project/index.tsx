0fde537cb5c293553298dd014c09902997c3433c
Mario Elvio <juka_mebaj@hotmail.com>
2022-08-28 02:46:28 -0300

    refactor: Multi-language types.

--- public/src/pages/Portfolio/Project/index.tsx
+++ public/src/pages/Portfolio/Project/index.tsx
@@ -16,12 +16,22 @@ export const Project = (props: Props) => {
   const { theme } = useContext(Context);
   const lang: string = theme.lang.status;
 
+  const project = {
+    name: props.name,
+    button: pageData.button.viewMore[ lang as keyof typeof pageData.button.viewMore ],
+    img: {
+      src: props.src,
+      alt: props.alt
+    },
+    url: props.url
+  }
+
   return (
     <C.Project>
-      <img src={props.src} alt={props.alt} />
+      <img src={project.img.src} alt={project.img.alt} />
       <div className="project-info">
-        <h3>{props.name}</h3>
-        <Link to={`/portfolio/${props.url}`}>{pageData.button.viewMore[lang]}</Link>
+        <h3>{project.name}</h3>
+        <Link to={`/portfolio/${project.url}`}>{project.button}</Link>
       </div>
     </C.Project>
   );
